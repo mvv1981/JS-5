1469511dda4559db56a3be6feb61b19879440fae
mvv1981 <mvv1981@users.noreply.github.com>
2019-03-26 07:49:37 +0400

    Create checks.js

---
+++ checks.js
@@ -0,0 +1,14 @@
+// Встроенный в Node.JS модуль для проверок
+var assert = require('assert');
+
+// Подключаем свою функцию
+var normalizeHashTags = require('./index.js');
+
+assert.deepEqual(
+    normalizeHashTags(['web', 'pravo', 'JavaScript', 'Pravo', 'script', 'programming']),
+    'web, pravo, javascript, script, programming',
+    'Список "web, pravo, JavaScript, Pravo, script, programming"' +
+    ' содержит хэштеги "web, pravo, javascript, script, programming"'
+);
+
+console.info('OK!');
