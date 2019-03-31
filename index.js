7060afcc739f2a7c7ecacb3c210de3891afe8532
phil11971 <phil182@mail.ru>
2019-03-31 10:57:08 +0400

    made JS-5

--- index.js
+++ index.js
@@ -3,5 +3,14 @@
  * @returns {String}
  */
 module.exports = function (hashtags) {
-
+    var resultSet = [];
+    
+    for(var i = 0; i < hashtags.length; i++){
+        var hashtag = hashtags[i].toLowerCase();
+        
+        if (!resultSet.includes(hashtag)) {
+            resultSet.push(hashtag);
+        }
+    }
+    return resultSet.join(", ");
 };
